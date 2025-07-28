fec4dec98228105f19e8edd2196eba81195a8e73
HongQuan <hq7t1k58@gmail.com>
2025-07-28 16:47:40 +0700

    done process_monitor on mac, take check on linux and win

--- process_monitoring/main.go
+++ process_monitoring/main.go
@@ -8,7 +8,6 @@ import (
 	"os/exec"
 	"runtime"
 	"strings"
-	"time"
 )
 
 // Config cau hinh giam sat process
@@ -16,7 +15,7 @@ type MonitorConfig struct {
 	MonitorFolder    []string `json:"monitor_folder"`
 	FileExtensions   []string `json:"file_extensions"`
 	IgnoreFiles      []string `json:"ignore_files"`
-	BaseLineFile     string   `json:"baseline_file"`
+	BaseLineProcess  string   `json:"baseline_process"`
 	MonitorProcess   bool     `json:"monitor_process"`
 	ProcessToMonitor []string `json:"process_to_monitor"`
 }
@@ -43,13 +42,13 @@ func loadConfig(configPath string) error {
 }
 
 func loadBaseline() error {
-	if _, err := os.Stat(config.BaseLineFile); os.IsNotExist(err) {
+	if _, err := os.Stat(config.BaseLineProcess); os.IsNotExist(err) {
 		baseline = SystemBaseline{
 			KnownProcess: make(map[string]bool),
 		}
 		return nil
 	}
-	file, err := os.ReadFile(config.BaseLineFile)
+	file, err := os.ReadFile(config.BaseLineProcess)
 	if err != nil {
 		return fmt.Errorf("unable to read baseline file: %v", err)
 	}
@@ -64,7 +63,7 @@ func saveBaseline() error {
 	if err != nil {
 		return fmt.Errorf("unable to marshal baseline file: %v", err)
 	}
-	if err := os.WriteFile(config.BaseLineFile, data, 0644); err != nil {
+	if err := os.WriteFile(config.BaseLineProcess, data, 0644); err != nil {
 		return fmt.Errorf("unable to write baseline file: %v", err)
 	}
 	return nil
@@ -113,11 +112,18 @@ func getRunningProcesses() ([]string, error) {
 			if parts := strings.Split(line, "\""); len(parts) >= 2 {
 				processName := strings.TrimSpace(parts[1])
 				if processName != "" {
+					processName = strings.ToLower(processName)
+					processName = strings.TrimSuffix(processName, ".exe")
 					processes = append(processes, strings.ToLower(processName))
 				}
 			}
 		} else {
 			processName := strings.ToLower(line)
+			// xu ly lay ten process
+			if strings.Contains(processName, "/") {
+				parts := strings.Split(processName, "/")
+				processName = parts[len(parts)-1]
+			}
 			processes = append(processes, processName)
 		}
 	}
@@ -130,7 +136,7 @@ func checkProcesses() {
 		return
 	}
 	fmt.Println("Checking processes...")
-	newProcessesFound := false
+	//newProcessesFound := false
 
 	currentProcesses, err := getRunningProcesses()
 	if err != nil {
@@ -149,7 +155,7 @@ func checkProcesses() {
 
 		if runningProcesses[normalizedMonitoredProc] {
 			if !baseline.KnownProcess[normalizedMonitoredProc] {
-				newProcessesFound = true
+				//newProcessesFound = true
 				fmt.Printf("\nALERT: Monitored process is running: %s\n", monitoredProc)
 				fmt.Print("Do you want to allow this process? (y/n): ")
 
@@ -187,9 +193,9 @@ func checkProcesses() {
 	//		}
 	//	}
 	//}
-	if !newProcessesFound {
-		fmt.Println("No new processes detected")
-	}
+	//if !newProcessesFound {
+	//	fmt.Println("No new processes detected")
+	//}
 
 }
 
@@ -213,16 +219,16 @@ func main() {
 	//fmt.Printf("\n Monitoring %d folder \n", len(config.MonitorFolder))
 	//checkFiles()
 	checkProcesses()
-	checkInterval := 1 * time.Minute
-
-	ticker := time.NewTicker(checkInterval)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			checkProcesses()
-		}
-	}
+	//checkInterval := 1 * time.Minute
+	//
+	//ticker := time.NewTicker(checkInterval)
+	//defer ticker.Stop()
+	//
+	//for {
+	//	select {
+	//	case <-ticker.C:
+	//		checkProcesses()
+	//	}
+	//}
 
 }
